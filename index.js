517484c546c452172dc20c934288d3917ad59d74
ilham98 <ilhamshinohara@gmail.com>
2020-07-15 07:02:24 +0800

    update

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
+const path = require("path");
+
+app.use(express.static(process.cwd() + "/myapp/dist/myapp/"));
 
 app.get('/', (req, res) => {
-  return res.send('hello world');
+  return res.sendFile(path.join(__dirname + "/myapp/dist/myapp/index.html"));
 });
 
 app.listen(port, () => {
